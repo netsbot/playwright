f4f9085f1cdaf6bfb43b6aab2788e63e98fd126d
netsbit <mail@netsbit.tech>
2023-04-13 22:46:17 +0700

    added init

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 python -m pip install -r requirements.txt
-playwright install
-playwright install-deps
-sudo apt install build-essential
-sudo apt install libcurl4-openssl-dev libsqlite3-dev pkg-config git curl
+~/.local/bin/playwright install
+~/.local/bin/playwright install-deps
+sudo apt install -y build-essential
+sudo apt install -y libcurl4-openssl-dev libsqlite3-dev pkg-config git curl
 curl -fsS https://dlang.org/install.sh | bash -s dmd
 git clone https://github.com/abraunegg/onedrive.git
 cd onedrive
