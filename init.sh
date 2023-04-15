7cd1e833a780e6e18e57e58cb11daefa1e9d3cbc
netsbit <mail@netsbit.tech>
2023-04-15 20:23:53 +0700

    finish last commit 3

--- init.sh
+++ init.sh
@@ -12,3 +12,8 @@ curl -fsS https://dlang.org/install.sh | bash -s dmd
 git clone https://github.com/abraunegg/onedrive.git
 cd onedrive
 bash -c "source ~/dlang/dmd-2.103.0/activate;./configure;make clean;make;sudo make install"
+git clone https://github.com/Intika-Linux-Namespace/Netns-Exec.git
+cd Netns-Exec
+git submodule update --init
+make
+sudo make install
