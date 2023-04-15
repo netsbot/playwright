9ec14fb5fe9b92c7b5bd5f3d6e3e98f16c54e18f
netsbit <mail@netsbit.tech>
2023-04-15 20:11:52 +0700

    finish last commit 2

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
+sudo apt install -y python3 python-is-python3
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 python -m pip install -r requirements.txt
 ~/.local/bin/playwright install
 ~/.local/bin/playwright install-deps
-sudo apt install -y build-essential
+curl -o ~/.local/bin/wg-netns https://raw.githubusercontent.com/dadevel/wg-netns/main/wgnetns/main.py
+chmod +x ~/.local/bin/wg-netns
+sudo apt install -y build-essential expect tmux wireguard
 sudo apt install -y libcurl4-openssl-dev libsqlite3-dev pkg-config git curl
 curl -fsS https://dlang.org/install.sh | bash -s dmd
 git clone https://github.com/abraunegg/onedrive.git
